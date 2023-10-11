80cae2f0d8ab165eac4923c2d8bec4e487f4f689
raselahmedit09 <rasel.ict09@gmail.com>
2023-10-11 06:00:41 +0600

    API Created

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+[ApiController]
+[Route("api/[controller]")] // api/user
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _dataContext;
+    public UsersController(DataContext dataContext)
+    {
+        _dataContext = dataContext;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        return await _dataContext.Users.ToListAsync();
+    }
+
+}

a19fdb1ca30fe0d305b8244efe7a7b3e0f0f3ba5
raselahmedit09 <rasel.ict09@gmail.com>
2023-11-29 14:59:32 +0600

    add auth guard

---
+++ client/src/app/_guards/auth.guard.ts
@@ -0,0 +1,20 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AccountService } from '../modules/account/services';
+import { SharedService } from '../_services';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const accountService = inject(AccountService);
+  const sharedService = inject(SharedService);
+  const router = inject(Router);
+
+  const loginUser = accountService.getLoginUserData();
+
+  if (loginUser) {
+    return true;
+  } else {
+    router.navigate(['/login']);
+    sharedService.warningMsg('You shall not pass');
+    return false;
+  }
+};
